c9d870d01080ef50513c0a614533aad6aa188299
oluwanifemi01 <nifeboy4u@gmail.com>
2026-02-18 18:59:40 -0600

    Initial Commit

---
+++ Budgetly App/Budgetly_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budgetly_AppApp.swift
+//  Budgetly App
+//
+//  Created by Oluwanifemi Oloyede on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Budgetly_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
